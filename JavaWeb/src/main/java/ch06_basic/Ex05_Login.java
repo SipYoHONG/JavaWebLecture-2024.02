3b936c51b7714ecb1240f9c058ddac21e8331a75
hong sipyo <155404215+SipYoHONG@users.noreply.github.com>
2024-02-14 14:44:00 +0900

    02.14 수업 끝

---
+++ JavaWeb/src/main/java/ch06_basic/Ex05_Login.java
@@ -0,0 +1,67 @@
+package ch06_basic;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
+@WebServlet("/ch06/login")
+public class Ex05_Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher("/ch06/loginForm.jsp");
+		rd.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String uid = request.getParameter("uid");
+		String pwd = request.getParameter("pwd");
+		
+		String msg ,url;
+		RequestDispatcher rd = null;
+		if (!uid.equals("james")) {
+			msg = "잘못된 아이디입니다.";
+			url =  "/jw/ch06/login";
+			
+		} else if (!pwd.equals("1234")) {
+			msg = "패스워드가 틀렸습니다.";
+			url =  "/jw/ch06/login";
+		
+		} else {
+			msg = "제임스님 환영합니다.";
+			url =  "/jw/ch06/loginResult.jsp";
+		}
+		
+		rd = request.getRequestDispatcher("/ch06/alertMsg.jsp");
+		request.setAttribute("msg", msg);
+		request.setAttribute("url", url);
+		rd.forward(request, response);
+		
+//		RequestDispatcher rd = request.getRequestDispatcher("/ch06/loginResult.jsp");
+//		request.setAttribute("uid", uid);
+//		request.setAttribute("pwd", pwd);
+//		request.setAttribute("msg", msg);
+//		rd.forward(request, response);
+				
+//		String result = "";
+//
+//		if (!uid.equals("james")) {
+//			result = "잘못된 아이디입니다.";
+//		} else if (!pwd.equals("1234")) {
+//			result = "패스워드가 틀렸습니다.";
+//		} else {
+//			result = "제임스님 환영합니다.";
+//		}
+//		RequestDispatcher rd = request.getRequestDispatcher("/ch06/loginResult.jsp");
+//		request.setAttribute("result", result);
+//		rd.forward(request, response);	
+	}
+
+}
